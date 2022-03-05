98f23587e94751f6c6d2acf0f646e8fdc94e34bd
zachjrt <tho19091@byui.edu>
2022-03-04 18:55:15 -0700

    Commit

--- js/menu.js
+++ js/menu.js
@@ -34,6 +34,10 @@ const links = [
     {
       label: "Week8 notes",
       url: "../week8/index.html"
+    },
+    {
+      label: "Week9 notes",
+      url: "../week9/index.html"
     }
     
 ];
